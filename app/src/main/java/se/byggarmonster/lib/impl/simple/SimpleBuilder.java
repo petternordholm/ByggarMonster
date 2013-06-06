134bc7e5cffabf2469f7814f4c7eeebc16c38a82
Tomas Bjerre <tomasbjerre@yahoo.com>
2013-06-06 12:44:35 +0200

    Parsing memebers

--- app/src/main/java/se/byggarmonster/lib/impl/simple/SimpleBuilder.java
+++ app/src/main/java/se/byggarmonster/lib/impl/simple/SimpleBuilder.java
@@ -4,18 +4,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 import se.byggarmonster.lib.parser.JavaBaseListener;
+import se.byggarmonster.lib.parser.JavaParser.FieldDeclarationContext;
 import se.byggarmonster.lib.parser.JavaParser.FormalParameterDeclsContext;
+import se.byggarmonster.lib.parser.JavaParser.MemberDeclarationContext;
 import se.byggarmonster.lib.parser.JavaParser.QualifiedNameContext;
+import se.byggarmonster.lib.parser.JavaParser.VariableDeclaratorContext;
 
 public class SimpleBuilder extends JavaBaseListener {
 	/**
 	 * Name => Type
 	 */
 	private final Map<String, String> constructorParameters;
+	/**
+	 * Name => Type
+	 */
+	private final Map<String, String> memberAttributes;
 	private String packageName;
 
 	public SimpleBuilder() {
 		constructorParameters = new HashMap<String, String>();
+		memberAttributes = new HashMap<String, String>();
+	}
+
+	@Override
+	public void exitFieldDeclaration(final FieldDeclarationContext ctx) {
+		for (int i = 0; i < ctx.variableDeclarators().getChildCount(); i++) {
+			final VariableDeclaratorContext child = (VariableDeclaratorContext) ctx
+					.variableDeclarators().getChild(i);
+			final MemberDeclarationContext mDC = (MemberDeclarationContext) child
+					.getParent().getParent().getParent();
+			memberAttributes.put(child.getText(), mDC.type().getText());
+		}
 	}
 
 	/**
@@ -24,8 +43,6 @@ public class SimpleBuilder extends JavaBaseListener {
 	@Override
 	public void exitFormalParameterDeclsRest(
 			final se.byggarmonster.lib.parser.JavaParser.FormalParameterDeclsRestContext ctx) {
-		System.out.println("exiting exitFormalParameterDeclsRest "
-				+ ctx.getText());
 		constructorParameters.put(ctx.getText(),
 				((FormalParameterDeclsContext) ctx.getParent()).type()
 						.getText());
@@ -43,8 +60,15 @@ public class SimpleBuilder extends JavaBaseListener {
 	public String toString() {
 		final StringBuilder sb = new StringBuilder();
 		sb.append("package " + packageName + ";\n");
+
+		sb.append("Constructor parameters:\n");
 		for (final String name : constructorParameters.keySet())
 			sb.append(" " + name + "(" + constructorParameters.get(name) + ")");
+
+		sb.append("\nAttributes:\n");
+		for (final String name : memberAttributes.keySet())
+			sb.append(" " + name + "(" + memberAttributes.get(name) + ")");
+
 		return sb.toString();
 	}
 }
--- app/src/resources/simple/se/byggarmonster/test/stringHolderSrc.java
+++ app/src/resources/simple/se/byggarmonster/test/stringHolderSrc.java
@@ -1,10 +1,17 @@
 package se.byggarmonster.test;
 
 public class stringHolderSrc {
+	private final int myInt;
 	private final String myString;
 
-	public stringHolderSrc(final String myStringConstrParam) {
+	public stringHolderSrc(final String myStringConstrParam,
+			final int theOtherConstructorParam) {
 		this.myString = myStringConstrParam;
+		this.myInt = theOtherConstructorParam;
+	}
+
+	public int getMyInt() {
+		return myInt;
 	}
 
 	public String getMyString() {
