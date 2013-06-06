e2770cfb803dc502292e05c9ca7f953d4c0cf367
Tomas Bjerre <tomasbjerre@yahoo.com>
2013-06-07 01:43:58 +0200

    Adding test with setter methods

---
+++ app/src/resources/templates/se/byggarmonster/test/simple/TwoFinalAttributesOneParamInConstructorOneSetterSrc.java
@@ -0,0 +1,23 @@
+package se.byggarmonster.test.simple;
+
+public class TwoFinalAttributesOneParamInConstructorOneSetterSrc {
+	private int myInt;
+	private final String myString;
+
+	public TwoFinalAttributesOneParamInConstructorOneSetterSrc(
+	        final String myStringDiff) {
+		this.myString = myStringDiff;
+	}
+	
+	public void setMyInt(int myIntDiff) {
+		this.myInt = myIntDiff;
+    }
+
+	public int getMyInt() {
+		return myInt;
+	}
+
+	public String getMyString() {
+		return myString;
+	}
+}
---
+++ app/src/resources/templates/se/byggarmonster/test/simple/TwoFinalAttributesOneParamInConstructorOneSetterSrcBuilder.java
@@ -0,0 +1,23 @@
+package se.byggarmonster.test.simple;
+
+public class TwoFinalAttributesOneParamInConstructorOneSetterSrcBuilder {
+    private int myInt;
+    private String myString;
+
+    public TwoFinalAttributesOneParamInConstructorOneSetterSrc build() {
+    	TwoFinalAttributesOneParamInConstructorOneSetterSrc instance = new TwoFinalAttributesOneParamInConstructorOneSetterSrc(myString);
+        instance.setMyInt(myInt);
+        return instance;
+    }
+
+    public TwoFinalAttributesOneParamInConstructorOneSetterSrcBuilder withmyInt(final int myInt) {
+        this.myInt = myInt;
+        return this;
+    }
+
+    public TwoFinalAttributesOneParamInConstructorOneSetterSrcBuilder withmyString(final String myString) {
+        this.myString = myString;
+        return this;
+    }
+
+}
