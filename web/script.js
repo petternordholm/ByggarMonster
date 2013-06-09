ef4287250bb7f190c61f09417819974453f18016
Tomas Bjerre <tomasbjerre@yahoo.com>
2013-06-09 11:07:41 +0200

    Removing tabs

--- web/script.js
+++ web/script.js
@@ -2,19 +2,19 @@ $(document).ready(function(){
        $.base64.utf8encode = true;
        $('.submit').click(function () {
            var encoded = $.base64.btoa($('#encodestring').val());
-		allocate(encoded);
-		$.ajax({
+           allocate(encoded);
+           $.ajax({
                url: "/api/compile/"+encoded, 
                async: true,
                cache: true,
                success: function(data, textStatus, jqXHR) {
-				var $response = $(data);
-				var input = $.base64.atob($response["input"]);
-				var output = $.base64.atob($response["output"]);
-				success(input,output);
+                var $response = $(data);
+                var input = $.base64.atob($response["input"]);
+                var output = $.base64.atob($response["output"]);
+                success(input,output);
                },
                error: function(data, textStatus, jqXHR) {
-				alert(data)
+                alert(data);
                }
            });
        });
