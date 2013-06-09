5747eb9f87c28927c0c2560d2619f21e41b2c639
Tomas Bjerre <tomasbjerre@yahoo.com>
2013-06-09 10:08:42 +0200

    Initial commit of web interface

---
+++ web/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+       $.base64.utf8encode = true;
+       $('.submit').click(function () {
+           var encoded = $.base64.btoa($('#encodestring').val());
+		allocate(encoded);
+		$.ajax({
+               url: "/api/compile/"+encoded, 
+               async: true,
+               cache: true,
+               success: function(data, textStatus, jqXHR) {
+				var $response = $(data);
+				var input = $.base64.atob($response["input"]);
+				var output = $.base64.atob($response["output"]);
+				success(input,output);
+               },
+               error: function(data, textStatus, jqXHR) {
+				alert(data)
+               }
+           });
+       });
+});
+
+/*
+ * Create builder div with spinner. Using encoded as id.
+ */
+function allocate(encoded) {
+	
+}
+
+/*
+ * Replace builder div with compiled code. Identified by encoded id.
+ */
+function success(encoded,decoded) {
+	alert("success: "+encoded+" "+decoded);x
+}
